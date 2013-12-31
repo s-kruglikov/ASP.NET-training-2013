b93817a3628145f27ab86a38b8fb240fb40b541a
s-kruglikov <stas.kruglikov@gmail.com>
2013-12-31 16:44:52 +0300

    MvcBlogApplication v .1.0.0.25a
    1. Sql deployment script fixed

--- MvcBlogApplication/SqlScripts/Deploy.sql
+++ MvcBlogApplication/SqlScripts/Deploy.sql
@@ -73,13 +73,6 @@ CREATE TABLE [dbo].[webpages_Roles] (
     UNIQUE NONCLUSTERED ([RoleName] ASC)
 );
 
-CREATE TABLE [dbo].[webpages_Roles] (
-    [RoleId]   INT            IDENTITY (1, 1) NOT NULL,
-    [RoleName] NVARCHAR (256) NOT NULL,
-    PRIMARY KEY CLUSTERED ([RoleId] ASC),
-    UNIQUE NONCLUSTERED ([RoleName] ASC)
-);
-
 IF @@ERROR != 0
 BEGIN
 	ROLLBACK
